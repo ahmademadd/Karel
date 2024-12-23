0cd8c79afa7907223fd0baab7e3c4a483f72b87d
Ahmad Emad <ahmademad996.ae@gmail.com>
2024-12-24 00:30:01 +0300

    final

--- src/BlankKarel.java
+++ src/BlankKarel.java
@@ -1,10 +1,4 @@
-/*
- * File: BlankKarel.java
- * ---------------------
- * This class is a blank one that you can change at will.
- */
 import stanford.karel.*;
-
 public class BlankKarel extends SuperKarel {
 	public void run() {
 		startPosition();
